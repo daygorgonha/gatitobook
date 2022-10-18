81de1b6813e2f14c50d55dc031a722165b350986
Dayane Gorgonha <dayanebismark18@gmail.com>
2022-10-18 09:18:03 -0300

    Cadastro de usuario

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MensagemModule } from '../componentes/mensagem/mensagem.module';
 
 @NgModule({
@@ -18,7 +18,8 @@ import { MensagemModule } from '../componentes/mensagem/mensagem.module';
     CommonModule,
     HomeRoutingModule,
     FormsModule,
-    MensagemModule
+    MensagemModule,
+    ReactiveFormsModule
   ],
   exports: [HomeComponent],
 })
