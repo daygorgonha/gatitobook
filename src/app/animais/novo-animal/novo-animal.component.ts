f0bbc5e73044638696ceb5978008647cf48988d0
Dayane Gorgonha <dayanebismark18@gmail.com>
2022-10-21 17:12:41 -0300

    Correcao api

--- src/app/animais/novo-animal/novo-animal.component.ts
+++ src/app/animais/novo-animal/novo-animal.component.ts
@@ -1,5 +1,9 @@
-import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AnimaisService } from './../animais.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { HttpEventType } from '@angular/common/http';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-novo-animal',
@@ -12,13 +16,43 @@ export class NovoAnimalComponent implements OnInit {
   preview!: string;
   percentualConcluido = 0;
 
-  constructor() { }
+  constructor(
+    private animaisService: AnimaisService,
+    private formbuilder: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.formularioAnimal = this.formbuilder.group({
+      file: ['', Validators.required],
+      description: ['', Validators.maxLength(300)],
+      allowComments: [true],
+    });
   }
 
-  upload() {}
+  upload() {
+    const allowComments =
+      this.formularioAnimal.get('allowComments')?.value ?? false;
+    const description = this.formularioAnimal.get('description')?.value ?? '';
 
-  gravaArquivo(arquivo: any) {}
+    this.animaisService
+    .upload(description, allowComments, this.file)
+    .pipe(finalize(() => this.router.navigate(['animais'])))
+    .subscribe(event: HttpEventType<any>): void => {
+      if (event.type === HttpEventType.UploadProgress) {
+        const total = event.total ?? 1;
+        this.percentualConcluido = Math.round(100 * event.loaded)
+      }
+    }
+    (error) => console.log(error)
+  }
+
+  gravaArquivo(arquivo: any): void {
+    const [file] = arquivo?.files;
+    this.file = file;
+    const reader = new FileReader();
+    reader.onload = (event: any) => (this.preview = event.target.result);
+    reader.readAsDataURL(file);
+  }
 
 }
